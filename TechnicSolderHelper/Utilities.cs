de796c6619e08d74571158d29d94ab8c87f18f47
FRNathan13 <nathan130200@gmail.com>
2018-08-13 10:05:55 -0300

    Updated Utilities.cs

--- TechnicSolderHelper/Utilities.cs
+++ TechnicSolderHelper/Utilities.cs
@@ -2,12 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace TechnicSolderHelper
 {
 	public static class Utilities
 	{
-		public static string GetSlug
+		public static string GenerateSlug(this string text)
+		{
+			var slug = text.RemoveAccent();
+			slug = Regex.Replace(slug, @"[^a-z0-9\s-]", "");
+			slug = Regex.Replace(slug, @"\s+", " ").Trim();
+			slug = Regex.Replace(slug, @"\s", "-");
+
+			if (slug.Length >= 45)
+				slug = slug.Substring(0, 45);
+
+			return slug.ToLowerInvariant();
+		}
+
+		public static string RemoveAccent(this string text)
+		{
+			byte[] buffer = Encoding
+				.GetEncoding("Cyrillic")
+				.GetBytes(text);
+
+			return Encoding.ASCII.GetString(buffer);
+		}
 	}
 }
